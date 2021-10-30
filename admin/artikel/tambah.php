f46c624f367f1ce1217b368abe6183827d60f7b8
nekobiru <nekobirugame@gmail.com>
2021-10-30 19:53:01 +0700

    project

--- admin/artikel/tambah.php
+++ admin/artikel/tambah.php
@@ -5,16 +5,46 @@
     <title>head</title>
     <meta name="viewport" content="width=device-width,initial-scale=1" />
     <script src="ckeditor/ckeditor.js" type="text/javascript" charset="utf-8"></script>
+    <link rel="stylesheet" href="inc/css/bootstrap.min.css" type="text/css" media="all" />
+    <link rel="stylesheet" href="inc/css/custom.css" type="text/css" media="all" />
+    <link rel="stylesheet" href="inc/fontawesome-free-5.15.3-web/css/all.min.css" type="text/css" media="all" />
+</head>
+<body class="bg-light">
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark container">
+        <a class="navbar-brand" href="#">Pcraft7265</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="nav-content" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="navbar-nav text-center">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="./?navlink=home">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="./?navlink=home">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="./?navlink=home">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
 </head>
 <body>
-    <textarea cols="10" id="editor1" name="editor1" rows="10" data-sample-short></textarea>
+    <form action="" method="post" accept-charset="utf-8">
+        <input type="text" name="Judul" id="judul" />
+        <textarea cols="10" id="editor1" name="editor1" rows="10" data-sample-short></textarea>
+        <input type="submit" name="kirim" id="kirim" value="Simpan" />
+    </form>
     <script>
         CKEDITOR.replace('editor1', {
-            height: 500,
+            height: 480,
 
             filebrowserUploadUrl: 'ckeditor/ck_upload.php',
             filebrowserUploadMethod: 'form'
         });
     </script>
+    <script src="inc/js/jquery-3.6.0.min.js"></script>
+    <script src="inc/js/popper.min.js"></script>
+    <script src="inc/js/bootstrap.min.js"></script>
+    <script src="inc/fontawesome-free-5.15.3-web/js/all.min.js"></script>
 </body>
 </html>
--- home.php
+++ home.php
@@ -1,1 +1,31 @@
-ss
+        <div class="row">
+            <article>
+                <div class="row row-cols-1 row-cols-md-3 g-4">
+                    <div class="col">
+                        <div class="card h-100">
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRlvSLCqhE6k79DhonGlNuV80TnWeGMDlwlQ&usqp=CAU" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card h-100">
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRlvSLCqhE6k79DhonGlNuV80TnWeGMDlwlQ&usqp=CAU" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card h-100">
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRlvSLCqhE6k79DhonGlNuV80TnWeGMDlwlQ&usqp=CAU" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </article>
+        </div>
