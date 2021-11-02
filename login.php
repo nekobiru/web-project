9409602ba06b5a09e7fe1c096b345c33fecac296
nekobiru <nekobirugame@gmail.com>
2021-11-02 12:05:05 +0700

    update

--- login.php
+++ login.php
@@ -1,1 +1,26 @@
-ss
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <title>head</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1" />
+    <script src="ckeditor/ckeditor.js" type="text/javascript" charset="utf-8"></script>
+    <link rel="stylesheet" href="admin/inc/css/bootstrap.min.css" type="text/css" media="all" />
+    <link rel="stylesheet" href="admin/inc/css/form.css" type="text/css" media="all" />
+    <link rel="stylesheet" href="admin/inc/fontawesome-free-5.15.3-web/css/all.min.css" type="text/css" media="all" />
+</head>
+<body>
+        <div class="form">
+            <h3 class="title-form">Login</h3>
+            <form action="" method="post" accept-charset="utf-8">
+                <input type="text" name="username" id="username" placeholder="Username" />
+                <input type="password" name="password" id="password" placeholder="Password" />
+                <input type="submit" name="login" id="login" value="Login" />
+            </form>
+        </div>
+    <script src="admin/inc/js/jquery-3.6.0.min.js"></script>
+    <script src="admin/inc/js/popper.min.js"></script>
+    <script src="admin/inc/js/bootstrap.min.js"></script>
+    <script src="admin/inc/fontawesome-free-5.15.3-web/js/all.min.js"></script>
+</body>
+</html>
